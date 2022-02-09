15ba6f5cd357d7592ec5d800757ab8baf9a2e3a9
robincrass <robincrass@tamu.edu>
2022-02-09 14:36:48 -0600

    Lab3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   resources :books do
     member do
       get :delete
     end
   end
   root 'books#index'
+  get '/users/:id', to: 'users#show'
+  get '/user_books/:id', to: 'user_books#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
